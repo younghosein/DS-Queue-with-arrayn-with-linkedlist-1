0d95e3e3e24dfed74bd968a48ace30b542ccc8e3
Hosein Mohamadpor <83702864+younghosein@users.noreply.github.com>
2023-05-22 01:13:43 +0330

    Add files via upload

---
+++ Queue_Example_1/Queue_Example_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Example_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue s = new Queue();
+
+            s.enqueue(26);
+            s.enqueue(72);
+            s.enqueue(98);
+            s.enqueue(51);
+            s.enqueue(83);
+            s.enqueue(13);
+            s.enqueue(56);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Queue Is : ");
+            s.print();
+            Console.ResetColor();
+            Console.WriteLine("\n");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            s.dequeue();
+            s.dequeue();
+            Console.WriteLine("Now Queue Is : ");
+            s.print();
+            Console.ResetColor();
+            Console.WriteLine("\n");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Queue_Example_1/Queue_Example_1/Queue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Example_1
+{
+    class Queue
+    {
+        public Node front;
+        public Node rear;
+
+        public Queue()
+        {
+            front = null;
+            rear = null;
+        }
+        public void print()
+        {
+            Node n = front;
+
+            if (front == null && rear == null)
+            {
+                Console.WriteLine("Queue Is Empty !");
+                return;
+            }
+            while (n != null)
+            {
+                Console.Write(n.data + " . ");
+                n = n.next;
+            }
+        }
+
+        public void enqueue(int d)
+        {
+            Node nn = new Node(d);
+
+            if (rear == null)
+            {
+                rear = nn;
+                front = nn;
+                return;
+            }
+            rear.next = nn;
+            rear = nn;
+        }
+        public void dequeue()
+        {
+            if (front == null)
+                return;
+
+            //Node dn = front;
+            Console.WriteLine($"{front.data} Is Deleted...");
+            front = front.next;
+
+            if (front == null)
+                rear = null;
+        }
+
+
+    }
+}
